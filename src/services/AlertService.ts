03d8be6d881892467f8d59762c30a3f4b652bcb0
Vinicius Smanioto <smaniotov@gmail.com>
2020-01-19 03:08:09 -0300

    improve services

--- src/services/AlertService.ts
+++ src/services/AlertService.ts
@@ -1,7 +1,27 @@
-import "reflect-metadata"
-import { Service } from "typedi"
+import 'reflect-metadata';
+import { Service } from 'typedi';
+import addMinutes from 'date-fns/addMinutes';
+import { AlertType } from '../types/Alert';
+import { CreateOrUpdateAlertInputType } from '../validators';
+import AlertRepository from '../repositories/AlertRepository';
 
 @Service()
-export class AlertService {
-    
+export default class AlertService {
+  private alertRepository: AlertRepository;
+
+  constructor(alertRepository: AlertRepository) {
+    this.alertRepository = alertRepository;
+  }
+
+  public findAlerts = async (alert: Partial<AlertType>) => this.alertRepository.findAll(alert);
+
+  public createAlert = async (alert: CreateOrUpdateAlertInputType) => {
+    const createdAt = new Date();
+    const nextMessage = addMinutes(createdAt, alert.delay);
+    const alertType = new AlertType({
+      ...alert, isActive: true, createdAt, nextMessage,
+    });
+    await this.alertRepository.insertOne(alertType);
+    return 'Ok';
+  };
 }
