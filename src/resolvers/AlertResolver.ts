94d2bb7043a46bdf06d9193597edf2cf0a1da071
Vinicius Smanioto <smaniotov@gmail.com>
2020-01-19 03:07:58 -0300

    improve resolvers

--- src/resolvers/AlertResolver.ts
+++ src/resolvers/AlertResolver.ts
@@ -1,20 +1,28 @@
-import { Inject, Service } from "typedi";
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
-import "reflect-metadata";
-import { AlertType } from "../types";
+import { Service } from 'typedi';
+import {
+  Arg, Mutation, Query, Resolver,
+} from 'type-graphql';
+import 'reflect-metadata';
+import { AlertType } from '../types';
+import { CreateOrUpdateAlertInputType } from '../validators';
+import { AlertService } from '../services';
 
 @Service()
 @Resolver(AlertType)
-export class AlertResolver {
-  constructor(private readonly context) {}
+export default class AlertResolver {
+  private readonly alertService: AlertService;
+
+  constructor(alertService: AlertService) {
+    this.alertService = alertService;
+  }
 
   @Query(() => AlertType)
-  async getAnything() {
-    console.log(this.context);
+  async getAlerts(@Arg('sendTo') sendTo: string) {
+    return this.alertService.findAlerts({ sendTo });
+  }
 
-    return {
-      name: "Testing",
-      email: "Testing"
-    } as AlertType;
+  @Mutation(() => String)
+  async createAlert(@Arg('alert') alert: CreateOrUpdateAlertInputType) {
+    return this.alertService.createAlert(alert);
   }
 }
