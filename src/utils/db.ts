c766e656d42650938a8dee45cd95b73041da996b
Vinícius Smanioto <smaniotov@gmail.com>
2020-01-26 23:53:18 -0300

    fix database for testing

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -4,9 +4,11 @@ import { CollectionEnum } from '../enums';
 
 dotenv.config();
 
-const { MONGO_URL = '' } = process.env;
+const { MONGO_URL = '', TEST = '' } = process.env;
 
-export const mongoClient = new MongoClient(MONGO_URL, {
+const url = `${MONGO_URL}/${TEST ? 'test' : 'alert'}`;
+
+export const mongoClient = new MongoClient(url, {
   useNewUrlParser: true, useUnifiedTopology: true,
 });
 
